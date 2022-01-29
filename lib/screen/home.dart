756517041ff21c7b06d1f807776c1f65abee84b2
Gempur Bayu <aji123zz@gmail.com>
2022-01-30 01:06:16 +0700

    add clock state & am/pm toggle

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_switch/flutter_switch.dart';
 
@@ -12,6 +13,9 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   
   bool isOn = false;
+  bool isAM = true;
+  List<bool> isSelected = List.generate(2, (index) => false);
+
 
   @override
   Widget build(BuildContext context) {
@@ -31,12 +35,12 @@ class _HomeState extends State<Home> {
       body: Column(
         children: [
           Padding(padding: const EdgeInsets.only(top: 80),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center ,
-            children: [
-              Transform.scale(
-                scale: 1.2,
-                child: FlutterSwitch(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center ,
+              children: [
+                Transform.scale(
+                  scale: 1.2,
+                  child: FlutterSwitch(
                     value: isOn,
                     activeColor: Color(0xff65D1BA),
                     inactiveColor: Colors.redAccent,
@@ -52,10 +56,75 @@ class _HomeState extends State<Home> {
                       });
                     }
                   ),
-              )
+                ),
               ],
+            ),  
           ),
-            
+          Padding(padding: const EdgeInsets.only(top: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("00",
+                style: TextStyle(
+                        fontSize: 54, 
+                        fontWeight: FontWeight.bold, 
+                        color: isOn ? Colors.green : Colors.black)
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text(":",
+                  style: TextStyle(fontSize: 54, 
+                  fontWeight: FontWeight.bold, 
+                  color: isOn ? Colors.green : Colors.black),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text("00",
+                style: TextStyle(
+                        fontSize: 54, 
+                        fontWeight: FontWeight.bold, 
+                        color: isOn ? Colors.green : Colors.black)
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: ToggleButtons(
+                    selectedColor: Color(0xff65D1BA),
+                    children: const [
+                      Text(
+                        'AM',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'PM',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                    onPressed: (int index) {
+                      setState(() {
+                        for (int buttonIndex = 0;
+                            buttonIndex < isSelected.length;
+                            buttonIndex++) {
+                          if (buttonIndex == index) {
+                            isAM = index == 0 ? true : false;
+                            isOn = false;
+                            isSelected[buttonIndex] = true;
+                          } else {
+                            isSelected[buttonIndex] = false;
+                          }
+                        }
+                      });
+                    },
+                    isSelected: isSelected,
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
